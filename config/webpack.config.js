48f71dab2f8caed5b57d2a179924d7c6707463bf
jimmyginga <jiminginga@gmail.com>
2021-08-14 22:56:51 +0100

    webpack updated

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     historyApiFallback: true,
     hot: true,
     inline: true,
-    port: 8080,
+    port: process.env.PORT || 8080,
     proxy: {
       '/api': process.env.API_URL || 'http://localhost:3000',
     },
