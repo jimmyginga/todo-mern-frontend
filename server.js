ac63c012e8b3e6ed00ca7bcbeb0831a99cf1e243
jimmyginga <jiminginga@gmail.com>
2021-08-16 06:37:25 +0100

    server

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8001;
 app.use(express.static(__dirname));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client'));
 });
 
 app.listen(PORT);
