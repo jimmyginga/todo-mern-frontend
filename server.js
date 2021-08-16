68dbf381faa4d1c687673a15bfa3f62051a653ae
jimmyginga <jiminginga@gmail.com>
2021-08-16 07:39:02 +0100

    full

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8001;
 app.use(express.static(__dirname));
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client'));
+  res.redirect('http://google.com');
 });
 
 app.listen(PORT);
